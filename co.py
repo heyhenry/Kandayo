bd0574367a544e30b59f4aabcf7ec9bfcfa55777
heyhenry <henryyy3@hotmail.com>
2024-08-20 20:12:16 +0800

    Added page title

--- co.py
+++ co.py
@@ -2,18 +2,25 @@ import tkinter as tk
 
 root = tk.Tk()
 
-yellow_frame = tk.Frame(root, width=600, height=80, bg='yellow')
-red_frame = tk.Frame(root, width=200, height=500, bg='red')
-blue_frame = tk.Frame(root, width=400, height=125, bg='blue')
-magenta_frame = tk.Frame(root, width=400, height=125, bg='magenta')
-orange_frame = tk.Frame(root, width=400, height=125, bg='orange')
-green_frame = tk.Frame(root, width=400, height=125, bg='green')
+yellow_frame = tk.Frame(root, width=600, height=80, bg='yellow', highlightbackground='black', highlightthickness=2, borderwidth=1, padx=5, pady=5)
+red_frame = tk.Frame(root, width=200, height=500, bg='red', highlightbackground='black', highlightthickness=1)
+blue_frame = tk.Frame(root, width=400, height=125, bg='blue', highlightbackground='black', highlightthickness=1)
+magenta_frame = tk.Frame(root, width=400, height=125, bg='magenta', highlightbackground='black', highlightthickness=1)
+orange_frame = tk.Frame(root, width=400, height=125, bg='orange', highlightbackground='black', highlightthickness=1)
+green_frame = tk.Frame(root, width=400, height=125, bg='green', highlightbackground='black', highlightthickness=1)
 
 yellow_frame.grid(row=0, columnspan=2)
+yellow_frame.grid_propagate(False)
 red_frame.grid(rowspan=4, column=0)
 blue_frame.grid(row=1, column=1)
 magenta_frame.grid(row=2, column=1)
 orange_frame.grid(row=3, column=1)
 green_frame.grid(row=4, column=1)
+
+co_lbl = tk.Label(yellow_frame, text='Characters Overview', font=('Helvetica', 18), bg='yellow')
+yellow_frame.columnconfigure(0, weight=1)
+yellow_frame.columnconfigure(1, weight=1)
+yellow_frame.columnconfigure(2, weight=1)
+co_lbl.grid(row=0, column=1)
 
 root.mainloop()
--- resources/resources_used.md
+++ resources/resources_used.md
@@ -2,3 +2,6 @@
 The following are the resources I have read, referred and learnt from to better understand the concepts, logic and code to achieve my vision for this application.
 
 - Adding border and border colour: https://www.geeksforgeeks.org/how-to-change-border-color-in-tkinter-widget/
+- Adding border efficiently: https://stackoverflow.com/questions/50691267/how-to-add-a-border-for-a-frame-in-python-tkinter
+- Dealing with ignored frame attributes: https://stackoverflow.com/questions/11413459/adding-label-to-frame-in-tkinter-ignores-the-attributes-of-frame
+- Using fonts: https://www.tutorialspoint.com/python/tk_fonts.htm
