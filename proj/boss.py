3d9444f00540ca9a26a86c47d7bea56d3f5847b3
heyhenry <henryyy3@hotmail.com>
2024-09-05 09:11:37 +0800

    implemented a boss class

---
+++ proj/boss.py
@@ -0,0 +1,6 @@
+class Boss:
+    def __init__(self, boss_name, boss_clear, boss_difficulty, party_size):
+        self.boss_name = boss_name
+        self.boss_clear = boss_clear
+        self.boss_difficulty = boss_difficulty
+        self.party_size = party_size
